eecd69fbb021c7dcb03487a8cce538556e18ef87
wahyukhairina <wkhairina.afzan@gmail.com>
2020-04-03 08:46:49 +0700

    Updates

---
+++ precache-manifest.02a20343528cd57be094263a193436d2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d57acdc5f02d28bd66d37b1913d6e342",
+    "url": "/totalin_reaactjs/index.html"
+  },
+  {
+    "revision": "ec4878cec4e1d3648f6a",
+    "url": "/totalin_reaactjs/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "1229b443f65e00db16bc",
+    "url": "/totalin_reaactjs/static/css/main.e47d085b.chunk.css"
+  },
+  {
+    "revision": "ec4878cec4e1d3648f6a",
+    "url": "/totalin_reaactjs/static/js/2.614369a9.chunk.js"
+  },
+  {
+    "revision": "eaaf63558baae456918a991a401208d7",
+    "url": "/totalin_reaactjs/static/js/2.614369a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1229b443f65e00db16bc",
+    "url": "/totalin_reaactjs/static/js/main.2af3705f.chunk.js"
+  },
+  {
+    "revision": "0dc02cffc59c537ed471",
+    "url": "/totalin_reaactjs/static/js/runtime-main.324010f5.js"
+  },
+  {
+    "revision": "fd92636084aeeb77ae7ec1f6b3971f25",
+    "url": "/totalin_reaactjs/static/media/bg.fd926360.png"
+  }
+]);
